0c4dbb78488e9952457fe59f8e24361a2358decb
DoktOrk <dr.peter.jankovics@gmail.com>
2016-05-29 02:16:53 +0200

    text correction

--- nummerkonverter.js
+++ nummerkonverter.js
@@ -5,7 +5,7 @@ function numValidator(data){
 }
 
 var numKonverter = function(){
-    var numString = prompt("Geben Sie, bitte eine Nummer von 0 bis 9999999999 ein! (Zahlensystem: 10)");
+    var numString = prompt("Geben Sie bitte, eine Nummer von 0 bis 9999999999 ein! (Zahlensystem: 10)");
     var num = parseInt(numString, 10);
     if (numValidator(num) && numString.length <= 10){
         return alert("The converter comes here!");
